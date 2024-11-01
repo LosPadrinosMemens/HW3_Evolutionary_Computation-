ef6212e59c50a14937e954075422f22f69818def
David Torres Treviño <d.torres.t01@gmail.com>
2024-10-31 21:19:57 -0600

    commit 6

--- PSO/pso.py
+++ PSO/pso.py
@@ -8,9 +8,15 @@ class PSO:
     def __init__(self, objective_function, n_var=2, config_file = "inputs/param_swarm.cfg"):
         self.load_config(config_file)
         self.objective_function = problems.FunctionFactory.select_function(objective_function, n_var)
-        self.particle_factory = ParticleFactory(objective_function)       
+        self.particle_factory = ParticleFactory(self.objective_function)       
         self.swarm = Swarm(self.swarm_size, self.particle_factory)
         self.lbest = Swarm(self.swarm_size, self.particle_factory)
+        self.gbest = self.particle_factory.create_particle()
+        # Initialization
+        self.gbest.initialize_location(np.infty)
+        self.swarm.initialize_swarm()
+        self.lbest.initialize_lbest_swarm()
+        self.generation_statistics = {}
         self.generation_t = 0
     
     def load_config(self, config_file):
@@ -24,7 +30,7 @@ class PSO:
         self.max_generations = int(config['SwarmSettings']['max_generations'])
 
     def pass_next_generation(self):
-        best_fitness =  self.gbest.evaluate_objective_function()
+        best_fitness =  self.gbest.get_objective_value()
         
         self.generation_t += 1
 
@@ -32,13 +38,14 @@ class PSO:
             "best_fitness": best_fitness
         }
 
-    def run(self):
-        self.swarm.initialize_swarm()
-        self.lbest.initialize_lbest_swarm()
-        gbest = self.particle_factory.create_particle()
-        gbest.initialize_location(np.infty)
+    def get_population_statistics(self):
+        """
+        Returns dictionary with information about best individual across generations
+        """
+        return self.generation_statistics
 
-        while self.generation_t  < self.max_generations():
+    def run(self):
+        while self.generation_t  < self.max_generations:
             for i in range(self.swarm.get_swarm_size()):
                 particle = self.swarm.get_particle_at(i)
                 particle_lbest = self.lbest.get_particle_at(i)
@@ -48,9 +55,9 @@ class PSO:
                     particle_lbest.set_objective_value(particle.get_objective_value())
                     
                 # Update the gBest position
-                if particle_lbest.get_objective_value() < gbest.get_objective_value():
-                    gbest.set_x(particle_lbest.get_x())
-                    gbest.set_objective_value(particle_lbest.get_objective_value())
+                if particle_lbest.get_objective_value() < self.gbest.get_objective_value():
+                    self.gbest.set_x(particle_lbest.get_x())
+                    self.gbest.set_objective_value(particle_lbest.get_objective_value())
                     
             # Random numbersfor the calculation of the velocity
             r1 = np.random.rand(self.objective_function.get_nvar(), 1)
@@ -64,7 +71,7 @@ class PSO:
                 for j in range(self.objective_function.get_nvar()):
                     particle_j = particle.get_x_at(j)
                     lbest_j =  lbest.get_x_at(j)
-                    gbest_j = gbest.get_x_at(j)
+                    gbest_j = self.gbest.get_x_at(j)
                     cognitive_comp = self.c1 * r1[j] * (lbest_j - particle_j)
                     social_comp = self.c2 * r2[j] * (gbest_j - particle_j)
                     veloc_0 = particle.get_velocity_at(j)
@@ -75,4 +82,3 @@ class PSO:
                     particle.set_x_at(value, j) # value is the new position for the j-th component of the particle
                 particle.evaluate_objective_function() # Calculate the objective value based on the new position of the particle
             self.pass_next_generation()
-        return gbest
